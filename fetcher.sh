b54f17c573eb24c1061fe439d9d70e8a770f7da3
illusionTBA <illusionztba@gmail.com>
2023-05-06 13:54:30 -0400

    add series

--- fetcher.sh
+++ fetcher.sh
@@ -2,9 +2,10 @@ date_str=$(date +%m_%d_%Y)
 
 # Construct the URL using the current date
 movie_url="https://files.tmdb.org/p/exports/movie_ids_${date_str}.json.gz"
-
+tv_url="https://files.tmdb.org/p/exports/tv_series_ids_${date_str}.json.gz"
 # Send the request using curl
 curl -O -v "$movie_url"
-
+curl -O -v "$tv_url"
 
 gunzip -c "movie_ids_${date_str}.json.gz" > "movie_ids.json"
+gunzip -c "tv_series_ids_${date_str}.json.gz" > "series_ids.json"
