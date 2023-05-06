3d0a3c6ef6f6cc675eb57ef6a589d6f4503d1a36
illusionTBA <illusionztba@gmail.com>
2023-05-06 13:50:06 -0400

    gunzip stuff

--- fetcher.sh
+++ fetcher.sh
@@ -1,10 +1,10 @@
 date_str=$(date +%m_%d_%Y)
 
 # Construct the URL using the current date
-url="https://files.tmdb.org/p/exports/movie_ids_${date_str}.json.gz"
+movie_url="https://files.tmdb.org/p/exports/movie_ids_${date_str}.json.gz"
 
 # Send the request using curl
-curl -O -v "$url"
+curl -O -v "$movie_url"
 
 
-gunzip "movie_ids_${date_str}.json.gz"
+gunzip -c "movie_ids_${date_str}.json.gz" > "movie_ids.json"
